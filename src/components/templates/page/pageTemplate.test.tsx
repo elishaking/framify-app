59a20a6481c5cea2b57827eafaf797ebbfbb1df9
King Elisha <ek.chibueze@gmail.com>
2020-07-14 14:42:51 +0100

    text(page-template): added tests

---
+++ src/components/templates/page/pageTemplate.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { PageTemplate } from ".";
+import { MemoryRouter } from "react-router-dom";
+
+describe("<PageTemplate />", () => {
+  let pageTemplate: any;
+  describe("Rendering", () => {
+    beforeEach(() => {
+      pageTemplate = render(
+        <MemoryRouter>
+          <PageTemplate>
+            <h1>New Page</h1>
+          </PageTemplate>
+        </MemoryRouter>
+      );
+    });
+
+    it("should render heading", () => {
+      const headingElement = pageTemplate.getByText(/Framify/i);
+      expect(headingElement).toBeInTheDocument();
+    });
+
+    it("should render footer", () => {
+      const footerText = pageTemplate.getByText(/Copyright/i);
+      expect(footerText).toBeInTheDocument();
+    });
+  });
+});
