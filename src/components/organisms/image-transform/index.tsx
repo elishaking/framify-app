0b7717cc0e6817c9b97c01c2230b45e609bd289a
King Elisha <ek.chibueze@gmail.com>
2020-07-14 16:20:24 +0100

    feat: added ImageTransform

---
+++ src/components/organisms/image-transform/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+// @ts-ignore
+import FreeTransform from "react-free-transform";
+
+import { style } from "./styles";
+
+interface TProps {
+  transformProps: any;
+  onUpdate: Function;
+}
+
+const Wrapper = styled.div`
+  ${style}
+`;
+
+export const ImageTransform = ({ transformProps, onUpdate }: TProps) => {
+  return (
+    <Wrapper>
+      <FreeTransform
+        onUpdate={(payload: any) => onUpdate(payload)}
+        {...transformProps}
+      >
+        <img
+          className="element"
+          style={{
+            width: transformProps.width,
+            height: transformProps.height,
+            ...transformProps.styles,
+          }}
+          src="https://avatarmaker.net/images/1.png"
+          alt="Nothing"
+        />
+      </FreeTransform>
+    </Wrapper>
+  );
+};
--- src/components/pages/upload/index.tsx
+++ src/components/pages/upload/index.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { PageTemplate } from "../../templates";
+import { ImageTransform } from "../../organisms";
 
 export const UploadPage = () => {
+  const [imageState, updateImageState] = useState({
+    x: 0,
+    y: 0,
+    scaleX: 1,
+    scaleY: 1,
+    width: 100,
+    height: 100,
+    angle: 0,
+    classPrefix: "tr",
+    styles: {},
+  });
+
+  const onUpdate = (payload: any) => {
+    updateImageState({
+      ...imageState,
+      ...payload,
+    });
+  };
+
   return (
     <PageTemplate>
-      <h3>Upload Image</h3>
+      <ImageTransform transformProps={imageState} onUpdate={onUpdate} />
     </PageTemplate>
   );
 };
